78111f6d276e75f234e949bf04da7b03187fe977
Flyingbiker <schoebel.aymeric@gmail.com>
2021-03-19 11:26:53 +0100

    modification cosmétique des formulaires

--- Views/addClientForm.php
+++ Views/addClientForm.php
@@ -2,19 +2,19 @@
     <h3>Formulaire d'ajout d'un client</h3>
     <div class="form-group">
         <label for="lastName">Nom</label>
-        <input type="text" name="lastName" >
+        <input type="text" name="lastName" class="form-control" placeholder="Nom">
     </div>
     <div class="form-group">
         <label for="firstName">Prénom</label>
-        <input type="text" name="firstName" >
+        <input type="text" name="firstName" class="form-control" placeholder="Prénom">
     </div>
     <div class="form-group">
         <label for="email">Email</label>
-        <input type="text" name="email" >
+        <input type="text" name="email" class="form-control" placeholder="Email">
     </div>
     <div class="form-group">
         <label for="age">Age</label>
-        <input type="text" name="age" >
+        <input type="text" name="age" class="form-control" placeholder="Age">
     </div>
     <button type="submit" class="btn btn-primary">Enregistrer le client</button>
 </form>
--- Views/addTravelForm.php
+++ Views/addTravelForm.php
@@ -2,15 +2,15 @@
     <h3>Nouveau voyage pour <?= $client->getFirstName() . ' - ' . $client->getLastName() ?></h3>
     <div class="form-group">
         <label for="departure">Départ</label>
-        <input type="text" name="departure" >
+        <input type="text" name="departure" class="form-control" placeholder="Départ">
     </div>
     <div class="form-group">
         <label for="arrival">Arrivée</label>
-        <input type="text" name="arrival" >
+        <input type="text" name="arrival" class="form-control" placeholder="Arrivée">
     </div>
     <div class="form-group">
-        <label for="date">Date départ</label>
-        <input type="date" name="date" >
+        <label for="date">Date de départ</label>
+        <input type="date" name="date" class="form-control" placeholder="Date de départ">
     </div>
 
     <button type="submit" class="btn btn-primary">Enregistrer le voyage</button>
